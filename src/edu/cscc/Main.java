5d9b78d5a208d32b52a11acc1b281c8b05c6f659
epierce14 <epierce14@student.cscc.edu>
2023-10-22 23:02:36 -0400

    Updated Main with initial JAXB unmarshaller.

--- src/edu/cscc/Main.java
+++ src/edu/cscc/Main.java
@@ -1,10 +1,26 @@
 package edu.cscc;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        String fileLocation = System.getProperty("user.dir") + File.separator + "employees.xml";
-        System.out.println(fileLocation);
+        try {
+            // Pass a variable list of classes to the class that it will use for
+            // marshalling and unmarshalling
+            JAXBContext context = JAXBContext.newInstance(EmployeesDocument.class);
+
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            Employees employees = (Employees) unmarshaller.unmarshal(new File("./java-3-xml-and-json-lab/employees.xml"));
+            employees.getEmployees().forEach(System.out::println);
+
+            //String fileLocation = System.getProperty("user.dir") + File.separator + "employees.xml";
+            //System.out.println(fileLocation);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 }
