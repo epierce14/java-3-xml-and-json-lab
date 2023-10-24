d96d0af860f942c5ac2af6969638030da36b9a62
epierce14 <epierce14@student.cscc.edu>
2023-10-23 21:05:32 -0400

    Updated main with for loop to validate records and write marshall invalid records to a separate file.

--- src/edu/cscc/Main.java
+++ src/edu/cscc/Main.java
@@ -2,6 +2,7 @@ package edu.cscc;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
@@ -15,7 +16,25 @@ public class Main {
             Unmarshaller unmarshaller = context.createUnmarshaller();
 
             Employees employees = (Employees) unmarshaller.unmarshal(new File("./employees.xml"));
+
             employees.getEmployees().forEach(System.out::println);
+
+            Employees invalidEmployees = new Employees();
+
+            for (int i = 0; i < employees.getEmployees().size(); i++) {
+                if (!(employees.getEmployees().get(i).getId().startsWith("2") && (employees.getEmployees().get(i).getId().endsWith("A") ||
+                        employees.getEmployees().get(i).getId().endsWith("S") || employees.getEmployees().get(i).getId().endsWith("M")))) {
+
+                    invalidEmployees.getEmployees().add(employees.getEmployees().get(i));
+                    employees.getEmployees().remove(employees.getEmployees().get(i));
+                    i--;
+                }
+            }
+
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.marshal(invalidEmployees, new File("invalidEmployees.xml"));
+
+
 
             //String fileLocation = System.getProperty("user.dir") + File.separator + "employees.xml";
             //System.out.println(fileLocation);
