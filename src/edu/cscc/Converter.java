89a3236be2b3a306c30f19286c9bb41e88c48429
epierce14 <epierce14@student.cscc.edu>
2023-10-26 21:39:19 -0400

    Added employee array to store records.

--- src/edu/cscc/Converter.java
+++ src/edu/cscc/Converter.java
@@ -24,7 +24,7 @@ public class Converter {
             empConverted.get(i).put("firstName", employees.getEmployees().get(i).getFirstName());
             empConverted.get(i).put("lastName", employees.getEmployees().get(i).getLastName());
             empConverted.get(i).put("payRate", employees.getEmployees().get(i).getPayRate());
-            empConverted.get(i).put("id", employees.getEmployees().get(i).getId().substring(0, 11));
+            empConverted.get(i).put("id", employees.getEmployees().get(i).getId().substring(0, employees.getEmployees().get(i).getId().length() - 1));
 
             if (employees.getEmployees().get(i).getId().endsWith("A")) {
                 empConverted.get(i).put("role", "associate");
@@ -43,7 +43,7 @@ public class Converter {
 
     public List<LinkedHashMap<String, Object>> getStores() {
 
-        List<LinkedHashMap<String, Object>> storeConverted = new ArrayList<LinkedHashMap<String, Object>>();
+        List<LinkedHashMap<String, Object>> storeConverted = new ArrayList<>();
 
         for (int i = 0; i < employees.getEmployees().size(); i++) {
 
@@ -56,8 +56,20 @@ public class Converter {
             storeConverted.get(i).put("zip", employees.getEmployees().get(i).getActiveStore().getZip());
         }
 
-        List<LinkedHashMap<String, Object>> deduped = storeConverted.stream().distinct().collect(Collectors.toList());
+        List<LinkedHashMap<String, Object>> dedupedStores = storeConverted.stream().distinct().collect(Collectors.toList());
+        ArrayList[] storeEmployees = new ArrayList[dedupedStores.size()];
 
-        return deduped;
+        for (int i = 0; i < dedupedStores.size(); i++) {
+            storeEmployees[i] = new ArrayList<String>();
+            for (int j = 0; j < employees.getEmployees().size(); j++) {
+                if (employees.getEmployees().get(j).getActiveStore().getStoreNumber().equals(dedupedStores.get(i).get("storeNumber"))) {
+
+                    storeEmployees[i].add(employees.getEmployees().get(j).getId().substring(0, employees.getEmployees().get(j).getId().length() - 1));
+                }
+                dedupedStores.get(i).put("employees", storeEmployees[i]);
+            }
+        }
+
+        return dedupedStores;
     }
 }
